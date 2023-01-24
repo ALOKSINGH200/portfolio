8df131a544879d8aec1222d874956b2820f8a324
alok <88269537+ALOKSINGH200@users.noreply.github.com>
2023-01-24 20:29:06 +0530

    Update script.js

--- AS Portfolio.html/script.js
+++ AS Portfolio.html/script.js
@@ -1,8 +1,8 @@
   console.log("script running...")
   document.querySelector('.Cross').style.display = 'none';
   document.querySelector('.Hamburger').addEventListener("click", () => {
-    document.querySelector(".sidebar").classList.toggle("sidebarGo");
-    if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
+    document.querySelector(".sidebar").classList.toggle("sidebarGO");
+    if (document.querySelector('.sidebar').classList.contains('sidebarGO')) {
       document.querySelector('.Ham').style.display = 'inline'
       document.querySelector('.Cross').style.display = 'none'
     }
